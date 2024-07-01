ce41b76645a5bac066f1b8c5e34bd254b20074dd
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-07-01 16:40:28 +0700

    first commit

---
+++ src/test/java/com/example/vinfan/VinfanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinfanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
