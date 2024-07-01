85f3b710dc9187e13e9d46a5b6654dddf5529a0f
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-07-01 16:38:20 +0700

    first commit

---
+++ src/main/java/com/example/vinfan/VinfanApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinfanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinfanApplication.class, args);
+    }
+
+}
