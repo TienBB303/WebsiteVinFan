3aa95719906a9f35f5956482c601f32edd61ec24
tuanpmph41739 <tuanpmph31739@fpt.edu.vn>
2024-07-02 21:54:21 +0700

    hihi

--- src/main/java/com/example/vinfan/controller/HomeController.java
+++ src/main/java/com/example/vinfan/controller/HomeController.java
@@ -8,21 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vinfan")
 public class HomeController {
 
-//    ===================LOGIN======================
+    //    ===================LOGIN======================
     @GetMapping("/login")
     public String login() {
         return "/login/index";
     }
+
     @GetMapping("/register")
     public String register() {
         return "/login/register";
     }
+
     @GetMapping("/forgot")
     public String forgot() {
         return "/login/forgot";
     }
+
     @GetMapping("/reset")
     public String reset() {
         return "/login/reset";
     }
+
+
 }
