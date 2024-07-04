f1bc36b16f95b903923c22412e8560f361fefc85
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-07-05 00:23:53 +0700

    tienBB up new code

--- src/main/java/com/example/vinfan/repository/SanPhamRepo.java
+++ src/main/java/com/example/vinfan/repository/SanPhamRepo.java
@@ -2,6 +2,9 @@ package com.example.vinfan.repository;
 
 import com.example.vinfan.entity.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SanPhamRepo extends JpaRepository<SanPham, Long> {
+    @Query("SELECT MAX(sp.ma) FROM SanPham sp")
+    String findMaxCode();
 }
