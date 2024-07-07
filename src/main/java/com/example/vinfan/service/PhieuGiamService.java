0f43e0eff2149518f8cb03fb67bcd8546b908fbe
Khoi <khoinaph31928@fpt.edu.vn>
2024-07-07 15:20:08 +0700

    khoiiiii

--- src/main/java/com/example/vinfan/service/PhieuGiamService.java
+++ src/main/java/com/example/vinfan/service/PhieuGiamService.java
@@ -1,8 +1,11 @@
 package com.example.vinfan.service;
 
+import com.example.vinfan.entity.PhieuGiam;
 import com.example.vinfan.repository.PhieuGiamRepo;
-import com.example.vinfan.repository.SanPhamRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
