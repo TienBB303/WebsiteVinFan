7fa79b52304b95f4bc2214cec394e4a109db2e50
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-10-12 15:51:44 +0700

    commit update san pham + service

--- src/main/java/com/example/datn/repository/SPCTRepo.java
+++ src/main/java/com/example/datn/repository/SPCTRepo.java
@@ -10,18 +10,15 @@ import java.math.BigDecimal;
 
 public interface SPCTRepo extends JpaRepository<SanPhamChiTiet, Long> {
 
+//  nhận các giá trị khi phân trang và tìm kiếm
     @Query("SELECT spct FROM SanPhamChiTiet spct JOIN spct.sanPham sp " +
             "WHERE (LOWER(sp.ten) LIKE LOWER(CONCAT('%', :query, '%')) " +
             "OR LOWER(sp.ma) LIKE LOWER(CONCAT('%', :query, '%')) " +
             "OR LOWER(sp.mo_ta) LIKE LOWER(CONCAT('%', :query, '%'))) " +
             "AND spct.gia BETWEEN :minPrice AND :maxPrice")
     Page<SanPhamChiTiet> searchProducts(String query, BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
-//    @Query("SELECT spct FROM SanPhamChiTiet spct JOIN spct.sanPham sp " +
-//            "WHERE (LOWER(sp.ten) LIKE LOWER(CONCAT('%', :query, '%')) " +
-//            "OR LOWER(sp.ma) LIKE LOWER(CONCAT('%', :query, '%')) " +
-//            "OR LOWER(sp.mo_ta) LIKE LOWER(CONCAT('%', :query, '%'))) " +
-//            "AND (spct.gia BETWEEN :minPrice AND :maxPrice) " +
-//            "AND sp.trang_thai = :trangThai")
-//    Page<SanPhamChiTiet> searchProducts(String query, BigDecimal minPrice, BigDecimal maxPrice, Boolean trangThai, Pageable pageable);
 
+//  Tìm sản phẩm cso giá cao nhất hiện tại
+    @Query("SELECT MAX(spct.gia) FROM SanPhamChiTiet spct")
+    BigDecimal findMaxPrice();
 }
