b9c19c51a3ef91f99f3bce089575ab906980699f
Khoi <khoinaph31928@fpt.edu.vn>
2024-10-29 12:37:27 +0700

    bán hàng tại quầy

--- src/main/java/com/example/datn/controller/BanHangController.java
+++ src/main/java/com/example/datn/controller/BanHangController.java
@@ -1,14 +1,35 @@
 package com.example.datn.controller;
-
+import com.example.datn.entity.CartItem;
+import com.example.datn.entity.CartItemRequest;
+import com.example.datn.entity.SanPhamChiTiet;
+import com.example.datn.repository.SPCTRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 @Controller
 @RequestMapping("/ban-hang")
 public class BanHangController {
+
+    @Autowired
+    private SPCTRepo spctRepo;
+
     @GetMapping("/index")
-    public String trangBan() {
+    public String trangBan(Model model) {
+        List<SanPhamChiTiet> danhSachSanPham = spctRepo.findAll();
+        System.out.println("Danh sách sản phẩm: " + danhSachSanPham); // Debug line
+        model.addAttribute("danhSachSanPham", danhSachSanPham);
         return "admin/ban_hang_tai_quay/index";
     }
 }
