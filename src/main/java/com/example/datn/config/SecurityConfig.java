1b6a50eac6958fa9eedc61ad6b331831498072cc
anhq792004 <nlquocanh204@gmail.com>
2024-12-17 23:08:26 +0700

    Bán hàng tại quầy, hóa đơn thêm xóa sản phẩm trong hóa đơn

--- src/main/java/com/example/datn/config/SecurityConfig.java
+++ src/main/java/com/example/datn/config/SecurityConfig.java
@@ -20,17 +20,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/ban-hang-tai-quay/**", "/cart/**", "admin/**")
+                        .ignoringRequestMatchers("/ban-hang-tai-quay/**", "/cart/**", "admin/**","/hoa-don/**")
                 )
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/login", "/logout","/forgot-password","/reset-password","/error","/admin/product-catalog","/register").permitAll()
+                                .requestMatchers("/", "/login", "/logout", "/forgot-password", "/reset-password", "/error", "/admin/product-catalog", "/register").permitAll()
 //                        .requestMatchers("/admin/product-catalog").hasAnyAuthority("ROLE_KHACHHANG")
-                        .requestMatchers("/thong-ke/index","/admin/nhan-vien/**").hasAnyAuthority("Quản lý")
-                        .requestMatchers("/admin/khach-hang/**", "/trang-ca-nhan/index", "/ban-hang-tai-quay/ban-hang", "/hoa-don/**","/admin/phieu-giam/**").hasAnyAuthority("Nhân viên bán hàng","Quản lý")
-                        .requestMatchers(HttpMethod.POST, "/ban-hang-tai-quay/**", "/cart/**").permitAll()  // Cho phép POST
-                        .requestMatchers(HttpMethod.PUT, "/ban-hang-tai-quay/**", "/cart/**").permitAll()   // Cho phép PUT
-                        .requestMatchers(HttpMethod.DELETE, "/ban-hang-tai-quay/**", "/cart/**").permitAll()// Cho phép DELETE
-                        .anyRequest().authenticated()
+                                .requestMatchers("/thong-ke/index", "/admin/nhan-vien/**").hasAnyAuthority("Quản lý")
+                                .requestMatchers("/admin/khach-hang/**", "/trang-ca-nhan/index", "/ban-hang-tai-quay/ban-hang", "/hoa-don/**", "/admin/phieu-giam/**").hasAnyAuthority("Nhân viên bán hàng", "Quản lý")
+                                .requestMatchers(HttpMethod.POST, "/ban-hang-tai-quay/**", "/cart/**").permitAll()  // Cho phép POST
+                                .requestMatchers(HttpMethod.PUT, "/ban-hang-tai-quay/**", "/cart/**").permitAll()   // Cho phép PUT
+                                .requestMatchers(HttpMethod.DELETE, "/ban-hang-tai-quay/**", "/cart/**").permitAll()// Cho phép DELETE
+
+                                .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
